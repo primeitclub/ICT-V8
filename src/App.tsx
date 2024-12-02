4bebb0077038ee0e29e2a2e6ce4c796cf035aff5
Sujal Chitrakar <sujalchitrakar9851182571@gmail.com>
2024-12-02 20:58:39 +0545

    faq added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
 import ComingSoon from "./pages/coming-soon";
-
+import LandingPage from "./pages/landing-page";
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<ComingSoon />} />
+      <Route path="/landing-page" element={<LandingPage />} />
     </Routes>
   );
 }
---
+++ src/pages/landing-page.tsx
@@ -0,0 +1,9 @@
+import FAQSection from "../components/molecules/sections/Faq";
+
+export default function LandingPage() {
+  return (
+    <>
+      <FAQSection />
+    </>
+  );
+}
