577c2ef60b974da8814829caefe5bfdf653a27d0
tutorial <aditikasingh2001@gmail.com>
2025-03-20 19:21:05 +0545

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,17 @@ import ComingSoon from "./pages/coming-soon";
 import LandingPage from "./pages/landing-page";
 import EventsPage from "./pages/events-page";
 import ScrollToTop from "./components/wrappers/scroll-to-top";
-import Hero from "./components/heroPage/hero";
-import PosterSection from "./components/molecules/sections/poster-section";
-import FAQSection from "./components/molecules/sections/faq-section";
+
 
 export default function App() {
   return (
     <>
-    <Hero/>
-    <PosterSection/>
-    <FAQSection/>
-      {/* <ScrollToTop />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<ComingSoon />} />
         <Route path="/landing-page" element={<LandingPage />} />
         <Route path="/events" element={<EventsPage />} />
-      </Routes> */}
+      </Routes>
     </>
   );
 }
